5043239696338f31bae1cc56ae72066615c1c1b5
Rodrigo Alcántara <103437322+Roiander@users.noreply.github.com>
2024-09-05 19:51:51 -0500

    chore: configured environment variables.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+// Environment: development
+// This is the development environment configuration file.
+// It contains the configuration settings for the development environment.
+// Note: In real scenarios, this file is not committed to the source control.
+export const environment = {
+  production: false,
+  newsApiKey: '756479ca82f344c39cf832a2fd37e820'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// Environment: production
+// This is the production environment configuration file.
+// It contains the configuration settings for the production environment.
+// Note: In real scenarios, this file is not committed to the source control.
+export const environment = {
+  production: true,
+  newsApiKey: '756479ca82f344c39cf832a2fd37e820'
+};
